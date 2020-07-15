8012a5d727ad893e55aa78acf8cf9cfe150f67eb
xenock <miguelmoracho@gmail.com>
2020-07-15 17:17:17 +0200

    feat: added spinner to list and separated busy state for buttons

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import { Table, TableRow, TableCell, Button } from '@marketgoo/ola'
+import { Table, TableRow, TableCell, Button, Spinner } from '@marketgoo/ola'
 import {
   removePlayerAPI,
   players as storedPlayers
@@ -13,18 +13,24 @@ axios.defaults.baseURL = 'http://localhost:3000'
 const PlayerList = _ => {
   const dispatch = useDispatch()
   const players = useSelector(storedPlayers)
+  const [synchronizing, setSynchronizing] = useState([])
 
   const handleDelete = player => {
+    setSynchronizing([...synchronizing, player.id])
     dispatch(removePlayerAPI(player))
       .then(response => {
         toast.success(`🦄 ${player.name} removed successfully!`)
+        setSynchronizing(synchronizing.filter(id => id !== player.id))
       })
       .catch(_ => {
         toast.error('🦄 There was an error...')
+        setSynchronizing(synchronizing.filter(id => id !== player.id))
       })
   }
 
-  return (
+  return !players.length ? (
+    <Spinner className={null} size='big' />
+  ) : (
     <Table caption={null} responsive sticky={false} stiky>
       <thead>
         <TableRow>
@@ -43,19 +49,22 @@ const PlayerList = _ => {
         </TableRow>
       </thead>
       <tbody>
-        {players &&
-          players.map(player => (
-            <TableRow key={player.id || Math.random()}>
-              <TableCell variant='multiline'>{player.name}</TableCell>
-              <TableCell variant='multiline'>{player.team}</TableCell>
-              <TableCell variant='numeric'>{player.score}</TableCell>
-              <TableCell variant='action'>
-                <Button variant='primary' onClick={() => handleDelete(player)}>
-                  Remove
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
+        {players.map(player => (
+          <TableRow key={player.id || Math.random()}>
+            <TableCell variant='multiline'>{player.name}</TableCell>
+            <TableCell variant='multiline'>{player.team}</TableCell>
+            <TableCell variant='numeric'>{player.score}</TableCell>
+            <TableCell variant='action'>
+              <Button
+                busy={synchronizing.includes(player.id) && 'Synchronizing'}
+                variant='primary'
+                onClick={() => handleDelete(player)}
+              >
+                Remove
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
       </tbody>
     </Table>
   )
